56d3d4f3e0e151211d74ca26ba391398fea9a113
MertJSX <mertJSX@proton.me>
2025-03-19 20:30:19 +0200

    Added cors

--- server/main.go
+++ server/main.go
@@ -7,11 +7,13 @@ import (
 	"github.com/MertJSX/forum-website/server/database"
 	"github.com/MertJSX/forum-website/server/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 	db, err := sql.Open("sqlite3", "./database/database.db")
 	if err != nil {
 		log.Fatal(err)
@@ -29,7 +31,7 @@ func main() {
 	})
 
 	app.Post("/register-user", func(c *fiber.Ctx) error {
-		return routes.HandleRegisterUser(c)
+		return routes.HandleRegisterUser(c, db)
 	})
 
 	app.Listen(PORT)
