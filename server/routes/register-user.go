98b7dbb9567eb6431f6f2031fbba3a249252c6ef
MertJSX <mertJSX@proton.me>
2025-04-27 11:48:33 +0300

    Modified register-user.go

--- server/routes/register-user.go
+++ server/routes/register-user.go
@@ -1,19 +1,74 @@
 package routes
 
 import (
+	"database/sql"
 	"fmt"
 
+	"github.com/MertJSX/forum-website/server/database"
 	"github.com/MertJSX/forum-website/server/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandleRegisterUser(c *fiber.Ctx) error {
-	reqBody := new(types.User)
+func HandleRegisterUser(c *fiber.Ctx, db *sql.DB) error {
+	user := new(types.User)
 
-	if err := c.BodyParser(reqBody); err != nil {
-		return err
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(400).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Bad request",
+		})
 	}
 
-	fmt.Println(reqBody.Name)
-	return c.JSON(reqBody)
+	fmt.Println(user)
+
+	if user.Name == "" || user.Email == "" || user.Password == "" {
+		return c.Status(400).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Missing required fields",
+		})
+	}
+
+	foundUser, err := database.SearchForUsers(db, *user, database.ByEmail)
+
+	if err != nil {
+		return c.Status(500).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Internal server error",
+		})
+	}
+
+	if foundUser != nil {
+		return c.Status(400).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Email already exists",
+		})
+	}
+
+	foundUser, err = database.SearchForUsers(db, *user, database.ByUsername)
+
+	if err != nil {
+		return c.Status(500).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Internal server error",
+		})
+	}
+
+	if foundUser != nil {
+		return c.Status(400).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Username already exists",
+		})
+	}
+
+	if len(user.Password) < 7 {
+		return c.Status(400).JSON(types.ErrorResponse{
+			IsError:  true,
+			ErrorMsg: "Password too short",
+		})
+	}
+
+	database.CreateNewUser(db, *user)
+
+	return c.SendString("User has created!")
+
 }
