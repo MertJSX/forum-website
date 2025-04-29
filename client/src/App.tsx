e99aaaa274322a5662a32760a6cd5d93c96170a4
MertJSX <mertJSX@proton.me>
2025-04-29 18:45:48 +0300

    Added new routes to App.jsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
+import Logout from "./pages/Logout";
+import NotFoundPage from "./pages/NotFoundPage";
+import Explore from "./pages/Explore/Explore";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<Explore />} />
+        <Route path="/signup" element={<Profile />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
