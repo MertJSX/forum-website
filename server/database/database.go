d67d452b724254e761c023c45a8965be832b348e
MertJSX <mertJSX@proton.me>
2025-05-01 22:08:56 +0300

    Modified database

--- server/database/database.go
+++ server/database/database.go
@@ -23,15 +23,34 @@ func CreateUsersTable(db *sql.DB) {
 	}
 }
 
-func CreateForumsTable(db *sql.DB) {
+func CreatePostsTable(db *sql.DB) {
 	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS forums (
+	CREATE TABLE IF NOT EXISTS posts (
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		user_id TEXT,
 		title TEXT,
 		description TEXT,
 		created_at TEXT,
-		content TEXT
+		content TEXT,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}
+
+func CreateCommentsTable(db *sql.DB) {
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS comments (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		user_id TEXT,
+		post_id TEXT,
+		comment TEXT,
+		created_at TEXT,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+		FOREIGN KEY(post_id) REFERENCES posts(id)
 	);`
 	_, err := db.Exec(sqlStmt)
 	if err != nil {
@@ -78,6 +97,90 @@ func CreateNewUser(db *sql.DB, usr types.User) error {
 	return nil
 }
 
+func CreateNewPost(db *sql.DB, post *types.Post) error {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Begin transaction error: %w", err)
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO posts(user_id, title, description, created_at, content) values(?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Prepare statement error: %w", err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(post.UserId, post.Title, post.Description, post.CreatedAt, post.Content)
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Statement execute error: %w", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Transaction commit error: %w", err)
+	}
+	return nil
+}
+
+func CreateNewComment(db *sql.DB, comment *types.Comment) error {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Begin transaction error: %w", err)
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO comments(user_id, post_id, comment, created_at) values(?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Prepare statement error: %w", err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(comment.UserId, comment.PostId, comment.Comment, comment.CreatedAt)
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Statement execute error: %w", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("Transaction commit error: %w", err)
+	}
+	return nil
+}
+
+func GetPosts(db *sql.DB) ([]types.Post, error) {
+	rows, err := db.Query("SELECT * FROM posts ORDER BY created_at DESC")
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("GetPosts Error: %v", err)
+	}
+
+	defer rows.Close()
+
+	var posts []types.Post
+
+	for rows.Next() {
+		var post types.Post
+		if err := rows.Scan(&post.ID, &post.UserId, &post.Title, &post.Description, &post.CreatedAt, &post.Content); err != nil {
+			fmt.Println(err)
+			return nil, fmt.Errorf("GetPosts Error %v: %v", post, err)
+		}
+		posts = append(posts, post)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("GetPosts Error %v: %v", posts, err)
+	}
+
+	return posts, nil
+}
+
 type SearchForUsersBy int
 
 const (
@@ -120,17 +223,11 @@ func SearchForUsers(
 			usr.Name, usr.Password)
 	}
 
-	fmt.Println("First Possible Error")
-
 	if err != nil {
 		fmt.Println(err)
 		return nil, fmt.Errorf("SearchForUsers Error %v: %v", usr, err)
 	}
 	defer rows.Close()
-
-	fmt.Println("Second Possible Error")
-
-	fmt.Println(rows.Columns())
 
 	for rows.Next() {
 		var usr types.User
@@ -140,8 +237,6 @@ func SearchForUsers(
 		}
 		foundList = append(foundList, usr)
 	}
-
-	fmt.Println("Third Possible Error")
 
 	if err := rows.Err(); err != nil {
 		return nil, fmt.Errorf("SearchForUsers %v: %v", usr, err)
