73f90eb6549e20b3bf84da70a95f89c6350afc7c
MertJSX <mertJSX@proton.me>
2025-05-01 21:59:54 +0300

    Modified Explore.tsx

--- client/src/pages/Explore/Explore.tsx
+++ client/src/pages/Explore/Explore.tsx
@@ -1,9 +1,12 @@
 import Navbar from "../../components/Navbar/Navbar"
+import CreateNewPost from "../../components/CreateNewPost/CreateNewPost"
 
 const Explore = () => {
   return (
     <div>
         <Navbar />
+        <CreateNewPost />
+        <h1 className="text-white text-2xl text-center">Explore newest posts!</h1>
     </div>
   )
 }
