4f842ec78e8493a9ce5078e519b1b274c3ce82d5
MertJSX <mertJSX@proton.me>
2025-05-03 23:10:20 +0300

    Modified client/src/utils/GetProfile.ts

--- client/src/utils/GetProfile.ts
+++ client/src/utils/GetProfile.ts
@@ -1,23 +1,26 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
-let GetProfile = new Promise(function(resolve, reject) {
-  const token = Cookies.get("token");
-  if (!token) {
-    return reject("Unauthorized");
-  }
-  axios
-    .get("/api/profile", { headers: { Authorization: token } })
-    .then((res) => {
-      return resolve(res.data);
-    })
-    .catch((err) => {
-      if (err.response.status === 401) {
-        return reject("Unauthorized");
-      }
-      return reject("Unknown error");
-    });
-    
-})
+let GetProfile = function(token?: string) {
+  return new Promise(function(resolve, reject) {
+    if (!token) {
+      return reject("Unauthorized");
+    }
+    axios
+      .get("/api/profile", { 
+        headers: { Authorization: token }
+      })
+      .then((res) => {
+        return resolve(res.data);
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          return reject("Unauthorized");
+        }else if (err.response.msg) {
+          return reject(err.response.msg);
+        }
+        return reject("Unknown error");
+      });
+  });
+};
 
 export default GetProfile;
