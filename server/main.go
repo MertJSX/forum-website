4d284a1967fcd726b8eed5a8676ed534b4eff8fb
MertJSX <mertJSX@proton.me>
2025-05-03 23:11:43 +0300

    Registered new routes to main.go

--- server/main.go
+++ server/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/MertJSX/forum-website/server/database"
 	"github.com/MertJSX/forum-website/server/middleware"
 	"github.com/MertJSX/forum-website/server/routes"
+	"github.com/MertJSX/forum-website/server/types"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -25,8 +29,19 @@ func main() {
 	database.CreateUsersTable(db)
 	database.CreatePostsTable(db)
 	database.CreateCommentsTable(db)
+	database.CreateUpvotesTable(db)
+	database.CreateFollowersTable(db)
 
-	var PORT string = ":3000"
+	fileData, err := os.ReadFile("./config.yml")
+	if err != nil {
+		log.Fatal("Error reading config file:", err)
+	}
+	var config types.ConfigFile
+	err = yaml.Unmarshal(fileData, &config)
+	if err != nil {
+		log.Fatalf("YAML parse error: %v", err)
+	}
+	var PORT string = fmt.Sprintf(":%d", config.Port)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
@@ -37,7 +52,7 @@ func main() {
 	})
 
 	app.Post("/login-user", func(c *fiber.Ctx) error {
-		return routes.HandleLoginUser(c, db)
+		return routes.HandleLoginUser(c, db, config.JWTSecret)
 	})
 
 	app.Get("/posts", func(c *fiber.Ctx) error {
@@ -48,12 +63,36 @@ func main() {
 		return routes.HandleGetPost(c, db)
 	})
 
+	app.Get("/posts/:id/comments", func(c *fiber.Ctx) error {
+		return routes.HandleGetPostComments(c, db)
+	})
+
+	app.Get("/userposts/:id", func(c *fiber.Ctx) error {
+		return routes.HandleGetUserPosts(c, db)
+	})
+
 	app.Use("/", func(c *fiber.Ctx) error {
-		return middleware.CheckAuth(c)
+		return middleware.CheckAuth(c, config.JWTSecret)
+	})
+
+	app.Get("/followed-users-posts", func(c *fiber.Ctx) error {
+		return routes.HandleGetFollowedUsersPosts(c, db)
+	})
+
+	app.Get("/profile/:id", func(c *fiber.Ctx) error {
+		return routes.HandleGetProfile(c, db)
 	})
 
 	app.Get("/profile", func(c *fiber.Ctx) error {
-		return routes.HandleGetProfile(c, db)
+		return routes.HandleGetProfileWithUserID(c, db, c.Locals("userID").(string))
+	})
+
+	app.Get("/upvote/:postID", func(c *fiber.Ctx) error {
+		return routes.HandleUpvotePost(c, db)
+	})
+
+	app.Get("follow/:id", func(c *fiber.Ctx) error {
+		return routes.HandleFollowUser(c, db)
 	})
 
 	app.Post("/create-post", func(c *fiber.Ctx) error {
@@ -62,6 +101,14 @@ func main() {
 
 	app.Post("/create-comment", func(c *fiber.Ctx) error {
 		return routes.HandleCommentPost(c, db)
+	})
+
+	app.Delete("/posts/:id", func(c *fiber.Ctx) error {
+		return routes.HandleDeletePost(c, db)
+	})
+
+	app.Put("/posts/:id", func(c *fiber.Ctx) error {
+		return routes.HandleEditPost(c, db)
 	})
 
 	app.Listen(PORT)
