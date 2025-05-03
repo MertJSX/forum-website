ba2c4e958ba076b2038a882fc0f8dab84e7d5e29
MertJSX <mertJSX@proton.me>
2025-05-04 00:44:20 +0300

    Some design changes

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -11,14 +11,14 @@ const SignIn = () => {
   const [emailOrUsername, setEmailOrUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [rememberMe, setRememberMe] = useState<boolean>(false);
-  const [errorMsg, setErrorMsg] = useState<string>("")
-  const navigate = useNavigate()
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const navigate = useNavigate();
 
   function getToken() {
     console.log(emailOrUsername);
     console.log(password);
 
-    setErrorMsg("")
+    setErrorMsg("");
 
     axios
       .post("/api/login-user", {
@@ -28,9 +28,9 @@ const SignIn = () => {
       })
       .then((data) => {
         if (data.data.error) {
-          setErrorMsg(data.data.msg)
+          setErrorMsg(data.data.msg);
           setPassword("");
-          return
+          return;
         }
         console.log(data.data.token);
         if (rememberMe) {
@@ -38,7 +38,7 @@ const SignIn = () => {
         } else {
           Cookies.set("token", data.data.token);
         }
-        navigate("/")
+        navigate("/");
         setEmailOrUsername("");
       })
       .catch((err) => {
@@ -47,75 +47,77 @@ const SignIn = () => {
   }
 
   return (
-    <div className="flex mt-28 m-auto justify-center items-center signup-container">
-      <div className="bg-gray-800 flex flex-col relative p-2 gap-2 border-r-2 border-blue-300 w-3/6 min-w-[700px] max-w-[1000px] h-[600px] rounded-l-2xl signup-child">
-        <div className="absolute z-10 left-25 top-44 select-none">
-          <h1 className="text-blue-200 text-5xl z-10 italic font-bold">
-            WELCOME AGAIN!
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex m-auto justify-center items-center signup-container">
+        <div className="bg-gray-800 flex flex-col relative p-2 gap-2 border-r-2 border-blue-300 w-3/6 min-w-[700px] max-w-[1000px] h-[600px] rounded-l-2xl signup-child">
+          <div className="absolute z-10 left-25 top-44 select-none">
+            <h1 className="text-blue-200 text-5xl z-10 italic font-bold">
+              WELCOME AGAIN!
+            </h1>
+          </div>
+          <video
+            src="/videos/signin1.mp4"
+            className="w-full top-0 left-0 absolute h-full object-cover rounded-l-2xl z-0 opacity-20"
+            muted
+            autoPlay
+            loop
+          />
+        </div>
+        <div className="bg-gray-800 flex flex-col p-2 gap-2 items-center min-w-[400px] w-2/6 max-w-[500px] h-[600px] rounded-r-2xl signup-child">
+          <div className="flex">
+            <Link
+              to="/"
+              className="flex flex-row justify-center items-center min-w-10 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
+            >
+              <IoArrowBackOutline />
+            </Link>
+            <Link
+              to="/signup"
+              className="flex flex-row justify-center items-center min-w-24 gap-2 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
+            >
+              <TiUserAdd /> Sign up
+            </Link>
+          </div>
+          <h1 className="text-blue-300 text-5xl font-bold mt-28 italic select-none">
+            SIGN IN
           </h1>
-        </div>
-        <video
-          src="/videos/signin1.mp4"
-          className="w-full top-0 left-0 absolute h-full object-cover rounded-l-2xl z-0 opacity-20"
-          muted
-          autoPlay
-          loop
-        />
-      </div>
-      <div className="bg-gray-800 flex flex-col p-2 gap-2 items-center min-w-[400px] w-2/6 max-w-[500px] h-[600px] rounded-r-2xl signup-child">
-        <div className="flex">
-          <Link
-            to="/"
-            className="flex flex-row justify-center items-center min-w-10 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
+          <input
+            className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
+            placeholder="Username / email"
+            value={emailOrUsername}
+            onChange={(e) => {
+              setEmailOrUsername(e.target.value);
+            }}
+            type="text"
+            autoFocus
+          />
+          <input
+            className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+          />
+          <Checkbox
+            label="Remember me"
+            checked={rememberMe.toString()}
+            onChange={() => {
+              setRememberMe(!rememberMe);
+            }}
+          />
+          <button
+            className="bg-blue-700 hover:bg-blue-600 cursor-pointer text-blue-200 w-3/4 text-lg font-bold rounded-2xl"
+            onClick={() => {
+              getToken();
+            }}
+            type="submit"
           >
-            <IoArrowBackOutline />
-          </Link>
-          <Link
-            to="/signup"
-            className="flex flex-row justify-center items-center min-w-24 gap-2 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
-          >
-            <TiUserAdd /> Sign up
-          </Link>
+            Sign in
+          </button>
+          <h2 className="text-red-400">{errorMsg ? errorMsg : null}</h2>
         </div>
-        <h1 className="text-blue-300 text-5xl font-bold mt-28 italic select-none">
-          SIGN IN
-        </h1>
-        <input
-          className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
-          placeholder="Username / email"
-          value={emailOrUsername}
-          onChange={(e) => {
-            setEmailOrUsername(e.target.value);
-          }}
-          type="text"
-          autoFocus
-        />
-        <input
-          className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          type="password"
-        />
-        <Checkbox
-          label="Remember me"
-          checked={rememberMe.toString()}
-          onChange={() => {
-            setRememberMe(!rememberMe);
-          }}
-        />
-        <button
-          className="bg-blue-700 hover:bg-blue-600 cursor-pointer text-blue-200 w-3/4 text-lg font-bold rounded-2xl"
-          onClick={() => {
-            getToken();
-          }}
-          type="submit"
-        >
-          Sign in
-        </button>
-        <h2 className="text-red-400">{errorMsg ? errorMsg : null}</h2>
       </div>
     </div>
   );
--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -19,127 +19,146 @@ const SignUp = () => {
     setInfo("");
     setErrInfo("");
     if (usernameRef.current?.classList.contains("bg-red-500")) {
-        usernameRef.current?.classList.remove("bg-red-500");
+      usernameRef.current?.classList.remove("bg-red-500");
     }
     if (emailRef.current?.classList.contains("bg-red-500")) {
-        emailRef.current?.classList.remove("bg-red-500");
+      emailRef.current?.classList.remove("bg-red-500");
     }
     if (passwordRef.current?.classList.contains("bg-red-500")) {
-        passwordRef.current?.classList.remove("bg-red-500");
+      passwordRef.current?.classList.remove("bg-red-500");
     }
-    axios.post("/api/register-user", {
+    axios
+      .post("/api/register-user", {
         username: username,
         email: email,
-        password: password
-    }).then(() => {
+        password: password,
+      })
+      .then(() => {
         setInfo("Account created successfully!");
         setUsername("");
         setEmail("");
         setPassword("");
-    }).catch((err: Error | AxiosError) => {
+      })
+      .catch((err: Error | AxiosError) => {
         if (axios.isAxiosError(err)) {
-            console.log(err.response?.data?.msg);
-            switch (err.response?.data?.msg) {
-                case "Username already exists":
-                    console.log(usernameRef.current);
-                    usernameRef.current?.classList.add("bg-red-500");
-                    setErrInfo("Username already exists!")
-                    break;
-                case "Email already exists":
-                    emailRef.current?.classList.add("bg-red-500");
-                    setErrInfo("Email already exists!")
-                    break;
-                case "Password too short":
-                    passwordRef.current?.classList.add("bg-red-500");
-                    setErrInfo("The password is too short!")
-                    break;
-                case "Missing required fields":
-                    setErrInfo("Please fill in all fields!")
-                    break;
-                default:
-                    setErrInfo("Unknown error!")
-                    break;
-            }
+          console.log(err.response?.data?.msg);
+          switch (err.response?.data?.msg) {
+            case "Username already exists":
+              console.log(usernameRef.current);
+              usernameRef.current?.classList.add("bg-red-500");
+              setErrInfo("Username already exists!");
+              break;
+            case "Email already exists":
+              emailRef.current?.classList.add("bg-red-500");
+              setErrInfo("Email already exists!");
+              break;
+            case "Password too short":
+              passwordRef.current?.classList.add("bg-red-500");
+              setErrInfo("The password is too short!");
+              break;
+            case "Missing required fields":
+              setErrInfo("Please fill in all fields!");
+              break;
+            default:
+              setErrInfo("Unknown error!");
+              break;
+          }
         } else {
-            console.error(err);
+          console.error(err);
         }
-    })
+      });
   }
 
   return (
-    <div className="flex mt-28 m-auto justify-center items-center signup-container">
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex m-auto justify-center items-center signup-container">
         <div className="bg-gray-800 flex flex-col relative p-2 gap-2 border-r-2 border-blue-300 w-3/6 min-w-[700px] max-w-[1000px] h-[600px] rounded-l-2xl signup-child">
-            <div className="absolute z-10 left-25 top-25 select-none">
-                <h1 className="text-blue-200 text-5xl z-10 italic font-bold">Become a part of us!</h1>
-                <ul  className="text-blue-100 text-2xl list-disc list-inside">
-                    <li>Make friends</li>
-                    <li>Socialize</li>
-                    <li>Ask questions to community</li>
-                    <li>Solve your problems</li>
-                </ul>
-            </div>
-            <video 
-            src="/videos/signup1.mp4" 
-            className="w-full top-0 left-0 absolute h-full object-cover rounded-l-2xl z-0 opacity-20" 
-            muted autoPlay loop />
+          <div className="absolute z-10 left-25 top-25 select-none">
+            <h1 className="text-blue-200 text-5xl z-10 italic font-bold">
+              Become a part of us!
+            </h1>
+            <ul className="text-blue-100 text-2xl list-disc list-inside">
+              <li>Make friends</li>
+              <li>Socialize</li>
+              <li>Ask questions to community</li>
+              <li>Solve your problems</li>
+            </ul>
+          </div>
+          <video
+            src="/videos/signup1.mp4"
+            className="w-full top-0 left-0 absolute h-full object-cover rounded-l-2xl z-0 opacity-20"
+            muted
+            autoPlay
+            loop
+          />
         </div>
         <div className="bg-gray-800 flex flex-col p-2 gap-2 items-center min-w-[400px] w-2/6 max-w-[500px] h-[600px] rounded-r-2xl signup-child">
-        <div className="flex">
-            <Link 
-                to="/"
-                className="flex flex-row justify-center items-center min-w-10 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
-            ><IoArrowBackOutline /></Link>
-            <Link 
-                to="/signin"
-                className="flex flex-row justify-center items-center min-w-24 gap-2 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
-            ><CiLogin /> Sign in</Link>
-        </div>
-        <h1 className="text-blue-300 text-5xl font-bold mt-28 italic select-none">SIGN UP</h1>
-        <input 
+          <div className="flex">
+            <Link
+              to="/"
+              className="flex flex-row justify-center items-center min-w-10 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
+            >
+              <IoArrowBackOutline />
+            </Link>
+            <Link
+              to="/signin"
+              className="flex flex-row justify-center items-center min-w-24 gap-2 text-white text-xl bg-gray-700 hover:bg-gray-600 p-1"
+            >
+              <CiLogin /> Sign in
+            </Link>
+          </div>
+          <h1 className="text-blue-300 text-5xl font-bold mt-28 italic select-none">
+            SIGN UP
+          </h1>
+          <input
             className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
             placeholder="Username"
             ref={usernameRef}
             value={username}
-            onChange={
-                (e: React.ChangeEvent<HTMLInputElement>) => {
-                    setUsername(e.target.value)
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setUsername(e.target.value);
             }}
-            type="text" autoFocus />
-        <input 
+            type="text"
+            autoFocus
+          />
+          <input
             className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
             placeholder="Email"
             ref={emailRef}
             value={email}
-            onChange={
-                (e: React.ChangeEvent<HTMLInputElement>) => {
-                    setEmail(e.target.value)
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value);
             }}
-            type="email" />
-        <input 
+            type="email"
+          />
+          <input
             className="text-center text-lg text-cyan-100 bg-gray-700 w-3/4 rounded-2xl outline-0 focus:bg-gray-600"
             placeholder="Password"
             ref={passwordRef}
             value={password}
-            onChange={
-                (e: React.ChangeEvent<HTMLInputElement>) => {
-                    setPassword(e.target.value)
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
             }}
-            type="password" />
-        <button 
-        className="bg-blue-700 hover:bg-blue-600 cursor-pointer text-blue-200 w-3/4 text-lg font-bold rounded-2xl"
-        onClick={() => {trySignUp()}}
-        type="submit">Sign up</button>
-        {
-            info ? 
-            <h1 className="text-lg text-blue-400">{info}</h1> :
-            errInfo ?
-            <h1 className="text-lg text-red-400">Error: {errInfo}</h1> : null
-        }
-
+            type="password"
+          />
+          <button
+            className="bg-blue-700 hover:bg-blue-600 cursor-pointer text-blue-200 w-3/4 text-lg font-bold rounded-2xl"
+            onClick={() => {
+              trySignUp();
+            }}
+            type="submit"
+          >
+            Sign up
+          </button>
+          {info ? (
+            <h1 className="text-lg text-blue-400">{info}</h1>
+          ) : errInfo ? (
+            <h1 className="text-lg text-red-400">Error: {errInfo}</h1>
+          ) : null}
+        </div>
+      </div>
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
